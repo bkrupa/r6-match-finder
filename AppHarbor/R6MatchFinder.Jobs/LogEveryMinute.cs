b7ac98eeabf2becc5a159e774041b53ee0362d63
bkrupa <bkrupa21@gmail.com>
2016-08-21 18:28:16 -0400

    Created a test logging job.

---
+++ AppHarbor/R6MatchFinder.Jobs/LogEveryMinute.cs
@@ -0,0 +1,37 @@
+﻿using Quartz;
+using R6MatchFinder.Common.Database;
+using R6MatchFinder.Common.Utilities;
+using R6MatchFinder.Jobs.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R6MatchFinder.Jobs
+{
+    [ScheduledJob(1)]
+    public class LogEveryMinute : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                using (R6Context dbContext = R6Context.New)
+                {
+                    dbContext.Log.Add(new Common.Database.Model.Log
+                    {
+                        Message = "Logged At " + DateTimeOffset.UtcNow
+                    });
+
+                    dbContext.SaveChanges();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Utilities.HandleException(ex);
+            }
+        }
+    }
+}
