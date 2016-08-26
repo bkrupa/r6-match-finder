c811f1cbfc676bd7c0feb09109885b45dab93676
bkrupa <bkrupa21@gmail.com>
2016-08-26 10:54:34 -0400

    Finally fixed the missing image url problem

--- AppHarbor/AppHarbor/Controllers/UserController.cs
+++ AppHarbor/AppHarbor/Controllers/UserController.cs
@@ -78,7 +78,7 @@ namespace R6MatchFinder.Controllers
 
             // Get here if the user doesn't have a "picture" claim
             HttpResponseMessage redirResponse = Request.CreateResponse(HttpStatusCode.TemporaryRedirect);
-            redirResponse.Headers.Location = new Uri(VirtualPathUtility.ToAbsolute("~/Images/Portrait_placeholder.png"));
+            redirResponse.Headers.Location = new Uri(VirtualPathUtility.ToAbsolute("~/Images/Portrait_placeholder.png"), UriKind.Relative);
             return redirResponse;
         }
 
